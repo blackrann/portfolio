6cdc80bc1ed0e1561250ba833f5462a805310e59
ImDeff2 <estoyloco534@gmail.com>
2026-01-11 16:13:09 +0100

    asd

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,13 @@
-import { withSentryConfig } from '@sentry/nextjs';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Esto ignora los errores de ESLint durante el build para asegurar que despliegue
     eslint: {
+        // Esto permite que el build termine aunque existan advertencias de linting (como las de las imágenes)
         ignoreDuringBuilds: true,
     },
-    // Opcional: ignora errores de TypeScript si los hubiera
     typescript: {
+        // Esto evita que errores menores de tipos detengan el despliegue
         ignoreBuildErrors: true,
     },
 };
 
-export default withSentryConfig(nextConfig, {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "javascript-mastery",
-    project: "javascript-nextjs",
-}, {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-});
+export default nextConfig;
